fe5609f34ad7e1c5395852bd01a416d77c8e3eeb
weixing <weixlost@163.com>
2016-08-23 21:59:35 +0800

    db password

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=weixin',
     'username' => 'root',
-    'password' => '',
+    'password' => 'jovision123$',
     'charset' => 'utf8',
 ];
